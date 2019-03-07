89231be4bc93afc853da198f0ed88f1bfa562d79
jjvilleg <jjvilleg@us.ibm.com>
2019-03-07 13:12:04 -0600

    Adding and configuring Integration Tests

---
+++ src/test/java/it/MongoIT.java
@@ -0,0 +1,16 @@
+package test.java.it;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+public class MongoIT {
+
+    public void mongoTest(){
+        assertTrue(true);
+    }
+}
