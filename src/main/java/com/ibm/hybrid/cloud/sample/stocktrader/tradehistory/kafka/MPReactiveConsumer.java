c5060d2f688a0ce10f4a706397bda002493fdc37
Raunak Shrestha <raunak.shrestha@gmail.com>
2021-03-16 18:51:25 -0400

    Add MongoDB connection to store stock purchases

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/tradehistory/kafka/MPReactiveConsumer.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/tradehistory/kafka/MPReactiveConsumer.java
@@ -1,19 +1,51 @@
 package com.ibm.hybrid.cloud.sample.stocktrader.tradehistory.kafka;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+
+import com.ibm.hybrid.cloud.sample.stocktrader.tradehistory.mongo.MongoConnector;
+import com.ibm.hybrid.cloud.sample.stocktrader.tradehistory.mongo.StockPurchase;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 @ApplicationScoped
 public class MPReactiveConsumer {
     
-    MPReactiveConsumer() {
+    private static MongoConnector mConnector = null;
+    private static String topic = null;
 
+    MPReactiveConsumer() {}
+
+    public void init(){
+        try {
+            mConnector = new MongoConnector();
+            topic = System.getenv("MP_MESSAGING_INCOMING_STOCKTRADER_TOPIC");
+            if (topic == null || topic.isEmpty()) {
+                topic = "stocktrader";
+            }
+        }
+        catch( NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Incoming("stocktrader")
     public void consume(String record) {
-        System.out.println("[INCOMING MESSAGE]: " + record);
+        StockPurchase sp = new StockPurchase(record);
+        try {
+            if (mConnector == null) {
+                init();
+            }
+            mConnector.insertStockPurchase(sp, topic);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean isHealthy() {
